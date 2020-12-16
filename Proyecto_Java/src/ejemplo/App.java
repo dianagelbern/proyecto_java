38f1cc5d4ef5bb84bedcdeaa63d6263e4039f889
“Dianagit config --global user.email “dianagonzalez4d@gmail.com <dianagonzalez4d@gmail.com>
2020-12-16 10:41:16 +0100

    Leer por teclado

--- Proyecto_Java/src/ejemplo/App.java
+++ Proyecto_Java/src/ejemplo/App.java
@@ -1,10 +1,19 @@
 package ejemplo;
 
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
+		
+		
 	}
-
+	public static String leerStringPorConsola() {
+		Scanner sc = new Scanner(System.in);
+		String result = sc.next();
+		sc.close();
+		return result;
+	}
 }
